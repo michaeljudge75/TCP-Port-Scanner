32241f73e5b36de7a95678063a4dae985327a6cd
Michael Judge <judgemichael04@gmail.com>
2025-11-13 00:07:33 -0500

    Redid main.rs for command line arguments

--- tcp-port-scanner/src/main.rs
+++ tcp-port-scanner/src/main.rs
@@ -1,20 +1,7 @@
-use std::io::{self, Write};
-
-pub fn prompt_input(prompt: &str) -> String{
-    print!("{}", prompt);
-    io::stdout().flush().unwrap();
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    //println!("{}",input);
-    input.trim().to_string()
-}
-
+#![allow(unused)]
+use tcp_port_scanner::cli::*;
+use clap::Parser;
 fn main() {
-    let target_input = String::new();
-    //Main Output
-    println!("TCP Port Scanner - Simple Rust Network Scanner");
-    println!("Please Enter a Target IP or Domain");
-    prompt_input(&target_input);
-    println!("Please Enter a Port Range to Scan");
-    prompt_input(&target_input);
+    parse_args(CliArgs::parse()).unwrap();
+
 }
