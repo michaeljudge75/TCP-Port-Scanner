5d6b78ca1efac66b9e782fc0b9b34f6de9f38860
Michael Judge <judgemichael04@gmail.com>
2025-11-14 14:50:22 -0500

    Added code to output in the format specified in output.rs

--- tcp-port-scanner/src/main.rs
+++ tcp-port-scanner/src/main.rs
@@ -1,7 +1,9 @@
 #![allow(unused)]
 use tcp_port_scanner::cli::*;
+use tcp_port_scanner::output::*;
 use clap::Parser;
 fn main() {
-    parse_args(CliArgs::parse()).unwrap();
-
+    //parse_args(CliArgs::parse()).unwrap();
+    let args = parse_args(CliArgs::parse()).unwrap();
+    print_scan_summary(&args.target, args.port_start, args.port_end);
 }
