b5acf2843d4d2a097b8ef3970ecdc608c30b5d52
Michael Judge <judgemichael04@gmail.com>
2025-12-01 22:43:45 -0500

    Added tests for open and closed port status

---
+++ tcp-port-scanner/tests/connect_test.rs
@@ -0,0 +1,25 @@
+use tcp_port_scanner::scanner::connect::scan_port;
+use tcp_port_scanner::results::{PortStatus, ScanError};
+
+use std::net::TcpListener;
+
+//Tests if Scanning an Open Port Works
+#[test]
+fn test_scan_open_port(){
+    let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind test socket");
+    let port = listener.local_addr().unwrap().port();
+
+    let result = scan_port("127.0.0.1", port as u32, 200);
+
+    assert!(matches!(result, Ok(PortStatus::Open)));
+}
+
+//Tests if Scanning a Closed Port Works
+static#[test]
+fn test_scan_closed_port(){
+   let result = scan_port("127.0.0.1", 65000, 200);
+
+    assert!(matches!(result, Ok(PortStatus::Closed)));
+}
+
+
