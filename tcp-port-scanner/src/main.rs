e87efeba376b978f3153688c47e176307e8ef4d3
michaeljudge75 <judgemichael04@gmail.com>
2025-12-03 02:06:18 -0500

    Added code to actually perform the network scan

--- tcp-port-scanner/src/main.rs
+++ tcp-port-scanner/src/main.rs
@@ -1,9 +1,26 @@
 #![allow(unused)]
 use tcp_port_scanner::cli::*;
 use tcp_port_scanner::output::*;
+use tcp_port_scanner::scan_engine::scan_range;
 use clap::Parser;
 fn main() {
-    //parse_args(CliArgs::parse()).unwrap();
     let args = parse_args(CliArgs::parse()).unwrap();
+
+    //Actually Performs Scan    
+    let results = scan_range(
+        &args.target,
+        args.port_start,
+        args.port_end,
+        args.timeout_ms,
+        args.rate_limit.unwrap(),
+    );
+
     print_scan_summary(&args.target, args.port_start, args.port_end);
+
+    //For Detailed Report   
+    /*
+    for (port, result) in results{
+        println!("{port}: {:?}", result);
+    }
+*/
 }
