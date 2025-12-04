33111bc4792188e76a00867d8c063f4554abe2b0
Michael Judge <judgemichael04@gmail.com>
2025-12-03 19:58:19 -0500

    Made changes to make rate limiting optional

--- tcp-port-scanner/src/main.rs
+++ tcp-port-scanner/src/main.rs
@@ -1,26 +1,32 @@
 #![allow(unused)]
+use clap::Parser;
 use tcp_port_scanner::cli::*;
 use tcp_port_scanner::output::*;
 use tcp_port_scanner::scan_engine::scan_range;
-use clap::Parser;
 fn main() {
     let args = parse_args(CliArgs::parse()).unwrap();
 
-    //Actually Performs Scan    
+    print_scan_summary(&args.target, args.port_start, args.port_end);
+
+    //Actually Performs Scan
     let results = scan_range(
         &args.target,
         args.port_start,
         args.port_end,
         args.timeout_ms,
-        args.rate_limit.unwrap(),
+        args.rate_limit,
     );
 
-    print_scan_summary(&args.target, args.port_start, args.port_end);
-
-    //For Detailed Report   
-    /*
-    for (port, result) in results{
-        println!("{port}: {:?}", result);
+    for (port, status) in results {
+        match status {
+            Ok(s) => println!("Port {}: {:?}", port, s),
+            Err(e) => println!("Port {}: {:?}", port, e),
+        }
     }
-*/
+    //For Detailed Report
+    /*
+        for (port, result) in results{
+            println!("{port}: {:?}", result);
+        }
+    */
 }
