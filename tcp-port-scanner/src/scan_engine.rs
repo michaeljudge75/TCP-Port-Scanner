aff192209ee1ccef0b90ac2b558256ec4f0d575e
Michael Judge <judgemichael04@gmail.com>
2025-12-03 19:57:47 -0500

    Made changes to make rate limiting optional

--- tcp-port-scanner/src/scan_engine.rs
+++ tcp-port-scanner/src/scan_engine.rs
@@ -1,18 +1,21 @@
-use crate::scanner::rate_limit::*;
-use crate::scanner::connect::scan_port;
 use crate::results::{PortStatus, ScanError};
+use crate::scanner::connect::scan_port;
+use crate::scanner::rate_limit::*;
 
 pub fn scan_range(
     host: &str,
     start_port: u32,
     end_port: u32,
     timeout_ms: u64,
-    rate_limit_per_sec: u64,
+    rate_limit_per_sec: Option<u64>,
 ) -> Vec<(u32, Result<PortStatus, ScanError>)> {
-    let mut limiter = RateLimiter::new(rate_limit_per_sec);
+    let mut limiter = rate_limit_per_sec.map(RateLimiter::new);
+    //RateLimiter::new(rate_limit_per_sec);
     let mut results = Vec::new();
-    for port in start_port..=end_port{
-        limiter.wait();
+    for port in start_port..=end_port {
+        if let Some(l) = limiter.as_mut() {
+            l.wait();
+        }
 
         let result = scan_port(host, port, timeout_ms);
 
